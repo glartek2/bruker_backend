c6636584384e2828f6bcde68982e692d81b68a39
dmarek03 <domarek@student.agh.edu.pl>
2025-05-10 17:12:58 +0200

    Added views

--- classroom_scheduler/views.py
+++ classroom_scheduler/views.py
@@ -1,96 +1,40 @@
-from django.shortcuts import render
-
-
-def home(request):
-    return render(request, 'homepage.html')
-
-
-
 from django.http import HttpResponse
-
-from django.urls import reverse_lazy
-from django.views import generic
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Building, Room, Equipment
+from .serializers import BuildingSerializer, RoomSerializer, EquipmentSerializer
+from .filters import DynamicJsonFilterBackend
+from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 
 def home(request):
     return HttpResponse('Classroom scheduler home page')
 
 
-# Buildings
-class BuildingListView(generic.ListView):
-    model = Building
+class BuildingViewSet(viewsets.ModelViewSet):
+    queryset = Building.objects.all()
+    serializer_class = BuildingSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    ordering_fields = ['name', 'address', 'department']
+    search_fields = ['name', 'address', 'department', 'description']
 
 
-#class BuildingDetailView(generic.DetailView):
-#    model = Building
+class EquipmentViewSet(viewsets.ModelViewSet):
+    queryset = Equipment.objects.all()
+    serializer_class = EquipmentSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['details']
 
 
-class BuildingCreateView(generic.CreateView):
-    model = Building
-    fields = ['name','address','department','description']
-    success_url = reverse_lazy('building-list')
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.select_related('building', 'equipment').all()
+    serializer_class = RoomSerializer
+    filter_backends = [DjangoFilterBackend, DynamicJsonFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = {
+        'capacity': ['exact', 'gte', 'lte'],
+        'room_number': ['exact', 'icontains'],
+        'building__name': ['exact', 'icontains'],
+    }
 
-
-class BuildingUpdateView(generic.UpdateView):
-    model = Building
-    fields = ['name','address','department','description']
-    success_url = reverse_lazy('building-list')
-
-
-class BuildingDeleteView(generic.DeleteView):
-    model = Building
-    success_url = reverse_lazy('building-list')
-
-
-# Rooms
-class RoomListView(generic.ListView):
-    model = Room
-
-
-#class RoomDetailView(generic.DetailView):
-#    model = Room
-
-
-class RoomCreateView(generic.CreateView):
-    model = Room
-    fields = ['building','equipment','capacity','room_number']
-    success_url = reverse_lazy('room-list')
-
-
-class RoomUpdateView(generic.UpdateView):
-    model = Room
-    fields = ['building','equipment','capacity','room_number']
-    success_url = reverse_lazy('room-list')
-
-
-class RoomDeleteView(generic.DeleteView):
-    model = Room
-    success_url = reverse_lazy('room-list')
-
-
-# Equipment
-class EquipmentListView(generic.ListView):
-    model = Equipment
-
-
-#class EquipmentDetailView(generic.DetailView):
-#    model = Equipment
-
-
-class EquipmentCreateView(generic.CreateView):
-    model = Equipment
-    fields = ['details']
-    success_url = reverse_lazy('equipment-list')
-
-
-class EquipmentUpdateView(generic.UpdateView):
-    model = Equipment
-    fields = ['details']
-    success_url = reverse_lazy('equipment-list')
-
-
-class EquipmentDeleteView(generic.DeleteView):
-    model = Equipment
-    success_url = reverse_lazy('equipment-list')
-
+    search_fields = ['room_number', 'building__name']
+    ordering_fields = ['capacity', 'room_number', 'building__name']
