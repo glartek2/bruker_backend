5ace3ae2d21db07ebd5faff92158da14fc5f37c7
dmarek03 <domarek@student.agh.edu.pl>
2025-05-10 17:13:14 +0200

    Registered views

--- classroom_scheduler/urls.py
+++ classroom_scheduler/urls.py
@@ -1,10 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
 
 app_name = 'home_module'
 
+router = DefaultRouter()
+router.register('buildings', views.BuildingViewSet)
+router.register('equipment', views.EquipmentViewSet)
+router.register('rooms', views.RoomViewSet)
 urlpatterns = [
     path('', views.home, name='home page'),
+    path('api/', include(router.urls))
 ]
-
-
