d543eeed04ab109eb0640ecab2b40526423c4b0e
dmarek03 <domarek@student.agh.edu.pl>
2025-05-14 23:39:03 +0200

    Registered views

--- users/urls.py
+++ users/urls.py
@@ -4,17 +4,11 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-
-]
-
-urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
-    # path('register/', views.register, name="register"),
-    # path('login/', views.custom_login, name='login'),
-    path('logout/', views.custom_logout, name='logout'),
-    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
-    path('reset_password/', views.password_reset_request, name='password_reset'),
-    path('reset/<uidb64>/<token>/', views.reset_password, name='reset')
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('activate/<uidb64>/<token>/', views.ActivateAccountView.as_view(), name='activate'),
+    path('reset_password/', views.ResetPasswordRequestView.as_view(), name='password_reset'),
+    path('reset/<uidb64>/<token>/', views.ResetPasswordConfirmationView.as_view(), name='password_reset_confirm')
 
 ]
