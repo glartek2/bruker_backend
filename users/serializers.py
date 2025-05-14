8c8dbf0f9cd36eda83923a3eb5212a39f5d4a68c
dmarek03 <domarek@student.agh.edu.pl>
2025-05-14 23:36:42 +0200

    Added register, login and reset password serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,39 @@
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from rest_framework.validators import UniqueValidator
+
 from .models import CustomUser
 
+
 class RegisterSerializer(serializers.ModelSerializer):
+
+    email = serializers.EmailField(
+        validators=[
+            UniqueValidator(queryset=CustomUser.objects.all(), message="Ten adres email jest już zajęty")
+        ]
+    )
+
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password']
-        # extra_kwargs = {'password': {'write_only': True}}
+        fields = ['username', 'email', 'password', 'password2']
+
+    def validate(self, attrs):
+
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({'password': "Hasła nie są zgodne"})
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+
+        )
+        user.set_password(validated_data['password'])
+        user.is_active = False
+        user.save()
         Token.objects.create(user=user)
         return user
 
@@ -21,3 +45,21 @@ class LoginSerializer(serializers.Serializer):
 
 class TokenResponseSerializer(serializers.Serializer):
     token = serializers.CharField()
+
+
+class ResetPasswordRequestSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+
+class ResetPasswordConfirmSerializer(serializers.Serializer):
+
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Hasła muszą być identyczne"})
+
+
+class MessageSerializer(serializers.Serializer):
+    message = serializers.CharField()
