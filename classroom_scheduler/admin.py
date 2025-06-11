2b0fd14aaf80e8aceace5210d180221a230c9985
dmarek03 <domarek@student.agh.edu.pl>
2025-06-11 13:53:06 +0200

    Registered models

--- classroom_scheduler/admin.py
+++ classroom_scheduler/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import ClassGroup, Room, Reservation, ReservationInfo, Equipment, Building
 # Register your models here.
+
+admin.site.register(ClassGroup)
+admin.site.register(Room)
+admin.site.register(Equipment)
+admin.site.register(Building)
+admin.site.register(Reservation)
+admin.site.register(ReservationInfo)
