69eac6f967c99fbbc1ecebbf9de5f4561db01166
michal1mroz <michalm12627@gmail.com>
2025-06-17 11:42:19 +0200

    Added automatic loading for groups and reservations

--- cli_tools/management/commands/populate_data.py
+++ cli_tools/management/commands/populate_data.py
@@ -1,8 +1,10 @@
 import json
 from django.core.management.base import BaseCommand
 from pathlib import Path
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-from classroom_scheduler.models import Building, Equipment, Room
+from classroom_scheduler.models import Building, ClassGroup, Equipment, ReservationInfo, Room
 from users.models import CustomUser
 
 
@@ -33,4 +35,49 @@ class Command(BaseCommand):
                 equipment, _ = Equipment.objects.get_or_create(**equipment_data)
                 room, created = Room.objects.get_or_create(building=building, equipment=equipment, **room_data)
                 self.stdout.write(self.style.SUCCESS(f"{'Created' if created else 'Skipped'} room: {room.room_number}"))
-        
+
+        with open(base_path / 'Groups.json') as f:
+            groups = json.load(f)
+            for group_data in groups:
+                members = [CustomUser.objects.get(username=name).pk for name in group_data.pop('members')]
+                class_rep = [CustomUser.objects.get(username=name).pk for name in group_data.pop('class_representatives')]
+                instructors = [CustomUser.objects.get(username=name).pk for name in group_data.pop('instructors')]
+
+                group, created = ClassGroup.objects.get_or_create(**group_data)
+                if created:
+                    for mem in members:
+                        group.members.add(mem)
+                    for rep in class_rep:
+                        group.class_representatives.add(rep)
+                    for inst in instructors:
+                        group.instructors.add(inst)
+                self.stdout.write(self.style.SUCCESS(f"{'Created' if created else 'Skipped'} class group: {group.name}"))
+
+        with open(base_path / 'Reservations.json') as f:
+            user = CustomUser.objects.get(username='admin')
+            client = APIClient()
+            client.force_authenticate(user=user)
+            reservations = json.load(f)
+            for res_data in reservations:
+                res_info_data = res_data.pop('reservation_info')
+
+                # create reservation info
+                user = CustomUser.objects.get(username=res_info_data.pop('user'))
+                group = ClassGroup.objects.get(name=res_info_data.pop('group'))
+                res_info, created1 = ReservationInfo.objects.get_or_create(user=user, group=group, **res_info_data)
+                self.stdout.write(self.style.SUCCESS(f"{'Created' if created1 else 'Skipped'} reservation info: {res_info.description}"))
+
+                room = Room.objects.get(room_number = res_data.pop('room'))
+
+                payload = {
+                    "room_id": room.pk,
+                    "reservation_info_id": res_info.pk,
+                    "date_times": res_data.pop('date_time')
+                }
+
+                resp= client.post(reverse("home_module:reservation-bulk-create-reservation"), data=payload, format='json')
+
+                if resp.status_code == 201:
+                    self.stdout.write(self.style.SUCCESS("Batch created successfully."))
+                else:
+                    self.stderr.write(self.style.ERROR(f"Error: {resp.status_code} - {resp.json()}"))
